ad7870c552bc9a19d6b24d3e7ebf86c6b46bb84c
yuanjiangting <yuanjiangting@baidu.com>
2020-03-08 12:27:30 +0800

    first commit

---
+++ 2.addTwoNumbers.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return addTwoNumbers(l1, l2, 0);
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) return null;
+        int p = 0, q = 0;
+        if (l1 != null) {
+            p = l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            q = l2.val;
+            l2 = l2.next;
+        }
+        ListNode head = new ListNode((p + q + carry) % 10);
+        head.next = addTwoNumbers(l1, l2, (p + q + carry) / 10);
+        return head;
+    }
+}
